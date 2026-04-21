a14960c4d9ccba3d1c15b92008adc8560c131f98
kelandep1-source <kelandep1@gmail.com>
2026-04-21 15:04:46 -0400

    Add files via upload

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,1 +1,19 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatDate(date: string | Date) {
+  return new Date(date).toLocaleDateString('en-US', {
+    month: 'short',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  })
+}
+
+export function formatNumber(num: number) {
+  return new Intl.NumberFormat('en-US').format(num)
+}
